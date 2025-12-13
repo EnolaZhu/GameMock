2d7371a4a6bbae0e5413019fe7307397c653eb6c
EnolaZhu <enola0314@gmail.com>
2025-12-13 15:55:20 +0800

    [Refactor] Refactor MatchRepository with protocol

--- GameMock/GameMock/Repository/MatchRepository.swift
+++ GameMock/GameMock/Repository/MatchRepository.swift
@@ -8,29 +8,44 @@
 import Foundation
 import Combine
 
-actor MatchRepository {
+protocol MatchRepositoryProtocol: Sendable {
+	// ViewModel'll call the protocol by different actors
+	func fetchInitialData() async -> [MatchWithOdds]
+	func startOddsStream() async -> AsyncStream<Odds>
+	func stopOddsStream() async
+}
+
+// MatchRepository.swift
+actor MatchRepository: MatchRepositoryProtocol {
+
 	private let dataService: MockDataService
 	private let webSocketSimulator: WebSocketSimulator
-	
-	init() {
-		self.dataService = MockDataService()
-		self.webSocketSimulator = WebSocketSimulator(dataService: dataService)
+
+	init(
+		dataService: MockDataService = MockDataService(),
+		webSocketSimulator: WebSocketSimulator? = nil
+	) {
+		self.dataService = dataService
+		self.webSocketSimulator = webSocketSimulator
+			?? WebSocketSimulator(dataService: dataService)
 	}
-	
+
 	func fetchInitialData() async -> [MatchWithOdds] {
 		let (matches, odds) = await dataService.generateMockData()
 		let oddsMap = Dictionary(uniqueKeysWithValues: odds.map { ($0.matchID, $0) })
-		
-		return matches.compactMap { match in
-			guard let odd = oddsMap[match.matchID] else { return nil }
-			return MatchWithOdds(match: match, odds: odd)
-		}.sorted { $0.match.startDate < $1.match.startDate }
+
+		return matches
+			.compactMap { match in
+				guard let odd = oddsMap[match.matchID] else { return nil }
+				return MatchWithOdds(match: match, odds: odd)
+			}
+			.sorted { $0.match.startDate < $1.match.startDate }
 	}
-	
+
 	func startOddsStream() async -> AsyncStream<Odds> {
 		await webSocketSimulator.startSimulation()
 	}
-	
+
 	func stopOddsStream() async {
 		await webSocketSimulator.stop()
 	}
