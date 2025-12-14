87fc16b1974fe51d31af519a189a0f9a599f23b3
EnolaZhu <enola0314@gmail.com>
2025-12-14 20:57:26 +0800

    [Refine] Remove the update time

--- GameMock/GameMock/Views/MatchRowView.swift
+++ GameMock/GameMock/Views/MatchRowView.swift
@@ -43,14 +43,14 @@ struct MatchRowView: View {
 						Text("•")
 							.font(.caption2)
 						
-						// 賠率更新時間
-						HStack(spacing: 4) {
-							Image(systemName: "arrow.clockwise")
-								.font(.caption2)
-							Text(formattedUpdateTime)
-								.font(.caption2)
-						}
-						.foregroundColor(.green)
+//						// 賠率更新時間
+//						HStack(spacing: 4) {
+//							Image(systemName: "arrow.clockwise")
+//								.font(.caption2)
+//							Text(formattedUpdateTime)
+//								.font(.caption2)
+//						}
+//						.foregroundColor(.green)
 					}
 					.foregroundColor(.secondary)
 				}
